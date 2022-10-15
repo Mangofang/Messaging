fb559b5ea8048852e6a6645fd5544de46bb23893
BuildTools <unconfigured@null.spigotmc.org>
2022-10-15 10:44:42 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static List<Socket> ClientProxSocketList = new List<Socket>();
+        public static string str = "";
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"),int.Parse("25565")));
+            socket.Listen(10);
+            Console.WriteLine("IP端口配置完毕");
+            ThreadPool.QueueUserWorkItem(ClientConnect =>
+            {
+                while (true)
+                {
+                    Socket proxSocket = socket.Accept();
+                    ClientProxSocketList.Add(proxSocket);
+                    Console.WriteLine("已与 " + proxSocket.RemoteEndPoint + "建立连接");
+                    ThreadPool.QueueUserWorkItem(ReceiveData =>
+                    {
+                        byte[] data = new byte[1024 * 1024];
+                        while (true)
+                        {
+                            int len = 0;
+                            try
+                            {
+                                len = proxSocket.Receive(data, 0, data.Length, SocketFlags.None);
+                                if (len == 0) break;
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("客户端:" + proxSocket.RemoteEndPoint.ToString() + "被迫断开连接");
+                                ClientProxSocketList.Remove(proxSocket);
+                                return;
+                            }
+
+                            if (len <= 0)
+                            {
+                                Console.WriteLine("客户端:" + proxSocket.RemoteEndPoint.ToString() + "断开连接");
+                                ClientProxSocketList.Remove(proxSocket);
+                                return;
+                            }
+
+                            str = Encoding.Default.GetString(data, 0, len);
+
+                            ThreadPool.QueueUserWorkItem(SendMsg =>
+                            {
+                                try
+                                {
+                                    foreach (Socket proxSocket_ in ClientProxSocketList)
+                                    {
+                                        if (proxSocket_.Connected)
+                                        {
+                                            byte[] data_ = Encoding.Default.GetBytes(str + "\n");
+                                            proxSocket_.Send(data_, 0, data_.Length, SocketFlags.None);
+                                        }
+                                    }
+                                }
+                                catch { }
+                            });
+                        }
+                    });
+                }
+            });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Socket Clientsocket;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(ServerConnect =>
+            {
+                Clientsocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                richTextBox1.Text = "正在连接服务器...";
+                try
+                {
+                    Clientsocket.Connect(IPAddress.Parse("127.0.0.1"), int.Parse("25565"));
+                    richTextBox1.Text += "\n你已连接到服务器\n";
+                }
+                catch (Exception ex)
+                {
+                    richTextBox1.Text += "\n连接失败，正在重试...";
+                    Thread.Sleep(5000);
+                    Form1_Load(this, e);
+                    return;
+                }
+                ThreadPool.QueueUserWorkItem(ReceiveData =>
+                {
+                    byte[] data = new byte[1024 * 1024];
+                    while (true)
+                    {
+                        int len;
+                        try
+                        {
+                            len = Clientsocket.Receive(data, 0, data.Length, SocketFlags.None);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("服务器:" + Clientsocket.RemoteEndPoint.ToString() + "被迫断开连接");
+                            return;
+                        }
+
+                        if (len <= 0)
+                        {
+                            Console.WriteLine("服务器:" + Clientsocket.RemoteEndPoint.ToString() + "断开连接");
+                            
+                            return;
+                        }
+                        string str = Encoding.Default.GetString(data,0,len);
+                        if (len != 0)
+                        {
+                            
+                            richTextBox1.AppendText(str);
+                        }
+                    }
+                });
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Clientsocket.Connected)
+            {
+                string ip = GetIP_();
+                string send_msg = ip + ":" + textBox1.Text;
+                byte[] data = Encoding.Default.GetBytes(send_msg);
+                Clientsocket.Send(data, 0, data.Length, SocketFlags.None);
+                textBox1.Text = "";
+            }
+        }
+        public static string GetIP_()
+        {
+            try
+            {
+                string hostName = Dns.GetHostName();
+                IPHostEntry iPHostEntry = Dns.GetHostEntry(hostName);
+                var addressV = iPHostEntry.AddressList.FirstOrDefault(q => q.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+                if (addressV != null)
+                    return addressV.ToString();
+                return "127.0.0.1";
+            }
+            catch (Exception ex)
+            {
+                return "127.0.0.1";
+            }
+
+        }
+    }
+}
